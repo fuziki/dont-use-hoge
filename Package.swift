f4b906cc84d4ac3c3766bb50095cff10c3ac333b
fuziki <hymne.shannon.projects@gmail.com>
2022-04-11 22:02:34 +0900

    update

--- Package.swift
+++ Package.swift
@@ -18,21 +18,21 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-            name: "lib_InternalSwiftSyntaxParser",
-            url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.6/lib_InternalSwiftSyntaxParser.xcframework.zip",
-            checksum: "88d748f76ec45880a8250438bd68e5d6ba716c8042f520998a438db87083ae9d"
-        ),
+//        .binaryTarget(
+//            name: "lib_InternalSwiftSyntaxParser",
+//            url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.6/lib_InternalSwiftSyntaxParser.xcframework.zip",
+//            checksum: "88d748f76ec45880a8250438bd68e5d6ba716c8042f520998a438db87083ae9d"
+//        ),
         .executableTarget(
             name: "dont-use-hoge",
             dependencies: [
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxParser", package: "swift-syntax"),
-                .target(name: "lib_InternalSwiftSyntaxParser"),
+//                .target(name: "lib_InternalSwiftSyntaxParser"),
             ],
             linkerSettings: [
-                .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"]),
-//                .unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", "@executable_path"]),
+//                .unsafeFlags(["-Xlinker", "-dead_strip_dylibs"]),
+                .unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", "@executable_path"]),
             ]),
         .plugin(
             name: "DontUseHogePlugin",
